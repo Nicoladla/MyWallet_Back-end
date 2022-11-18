5b43231f1c9c901822ff6b7e5ff3e50ed0f735bf
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-18 15:55:20 -0300

    Validação dos dados do usuário com Joi

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,22 @@
 import { v4 as uuid } from "uuid";
 import bcrypt from "bcrypt";
 
-export async function signUp(req, res) {}
+import { usersCollection, sessionsCollection } from "../database/db.js";
+import { userSchema } from "../schema/userSchema.js";
+
+export async function signUp(req, res) {
+  const user = req.body;
+
+  const { error } = userSchema.validate(user, { abortEarly: false });
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    return res.status(422).send({ message: errors });
+  }
+
+  
+
+  res.sendStatus(200);
+}
 
 export async function signIn(req, res) {}
 
