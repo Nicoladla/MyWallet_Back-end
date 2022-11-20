53f048252ce6a8fe7b4f3bcc799b1627cfefe667
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-20 08:49:10 -0300

    Adicionado o compo description no objeto de transação

--- src/schema/transactionSchema.js
+++ src/schema/transactionSchema.js
@@ -2,6 +2,7 @@ import joi from "joi";
 
 const transactionSchema = joi.object({
   value: joi.number().min(1).required(),
+  description: joi.string().min(3).max(60).required(),
   type: joi.string().valid("deposit", "withdraw").required(),
 });
 
